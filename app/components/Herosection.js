f25c45af71c6b9cd05f0b697fd95f7a778b968f9
Saksham511 <sakshamshrestha511@gmail.com>
2023-05-28 22:54:31 +0545

    created Herosection for main content

---
+++ app/components/Herosection.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const HeroSection = () => {
+    return(
+        <div>
+            
+        </div>
+    );
+  };
+  
+  export default HeroSection;
